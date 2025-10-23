873dad5d78a043182cbf44d1a83f7970a5b7c951
Mop321 <aweds200@gmail.com>
2025-10-23 23:04:36 +0300

    Calculator for my work (only for self use)

---
+++ settings.js
@@ -0,0 +1,58 @@
+const short = console.log;
+let hours = JSON.parse(localStorage.getItem("hours")) || [];
+let dates = JSON.parse(localStorage.getItem("date")) || [];
+const varibles = {
+  hoursInput: document.querySelector(".input1"),
+  dateInput: document.querySelector(".input2"),
+  clearInput: document.querySelector(".input3"),
+  submitHours: document.querySelector(".submit1"),
+  submitClear: document.querySelector(".submit3"),
+  resultDiv: document.querySelector(".results"),
+  finalDiv: document.querySelector(".result"),
+};
+const {
+  hoursInput,
+  dateInput,
+  clearInput,
+  submitHours,
+  submitClear,
+  resultDiv,
+  finalDiv,
+} = varibles;
+function clicks() {
+  submitHours.addEventListener("click", () => {
+    let hoursValue = Number(hoursInput.value);
+    let dateValue = dateInput.value;
+    if (hoursValue && dateValue) {
+      hours.push(hoursValue);
+      localStorage.setItem("hours", JSON.stringify(hours));
+      dates.push(dateValue);
+      localStorage.setItem("date", JSON.stringify(dates));
+      dateInput.value = "";
+      hoursInput.value = "";
+      result();
+    } else if (!hoursValue || !dateValue) {
+      document.body.innerHTML = "<h1>Error<h1/>";
+    }
+  });
+  submitClear.addEventListener("click", () => {
+    localStorage.clear();
+  });
+}
+
+function result() {
+  let html = "";
+  let total = 0;
+  hours.forEach((hour, index1) => {
+    dates.forEach((currentDate, index2) => {
+      if (index1 === index2) {
+        html += `<div class="result-item">Date: ${currentDate} - Hours: ${hour}</div>`;
+        total += hour * 40;
+      }
+    });
+  });
+  resultDiv.innerHTML = html;
+  finalDiv.innerHTML = `Total Paycheck: ${total}`;
+}
+result();
+clicks();
